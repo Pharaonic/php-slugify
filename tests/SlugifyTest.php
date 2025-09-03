9aae74759fb562649bb0392e6586d4306020e5ad
Moamen Eltouny <raggigroup@gmail.com>
2025-09-03 18:43:07 +0300

    Refactor SlugifyTest: Consolidate slug tests and remove redundant code

--- tests/SlugifyTest.php
+++ tests/SlugifyTest.php
@@ -2,30 +2,38 @@
 
 namespace Pharaonic\Slugify\Tests;
 
-use Pharaonic\Slugify\Slugify;
+use Pharaonic\Slugify\Facades\Slugify;
 use PHPUnit\Framework\TestCase;
 
 class SlugifyTest extends TestCase
 {
     public function testSlug()
     {
-        $expected = 'moamen-eltouny';
+        $this->assertSame('hello-world', slug('hello world'));
+        $this->assertSame('hello-world', slug('hello-world'));
+        $this->assertSame('hello-world', slug('hello_world'));
+        $this->assertSame('hello_world', slug('hello_world', '_'));
+        $this->assertSame('user-at-host', slug('user@host'));
+        $this->assertSame('سلام-دنیا', slug('سلام دنیا', '-'));
+        $this->assertSame('sometext', slug('some text', ''));
+        $this->assertSame('', slug('', ''));
+        $this->assertSame('', slug(''));
 
-        $this->assertSame($expected, Slugify::get('Moamen Eltouny'));
-    }
-
-    public function testSlugHelper()
-    {
-        $expected = 'moamen-eltouny';
-
-        $this->assertSame($expected, slug('Moamen Eltouny'));
+        $this->assertSame('there-is-faq-module-here', slug('There is FAQ module here'));
+        $this->assertSame('method-name-in-camel-case', slug('methodNameInCamelCase'));
+        $this->assertSame('class_name_in_pascal_case', slug('ClassNameIn-PascalCase', '_'));
     }
 
     public function testRuleManipulation()
     {
-        $expected = 'moeamen-eltoeuny';
-        Slugify::rule('ö', 'oe');
+        Slugify::rule('allh', 'allah');
+        $this->assertSame('bsm_allah', slug('بسم الله', '_', true));
 
-        $this->assertSame($expected, Slugify::get('Möamen Eltöuny'));
+        Slugify::rule('ö', 'oe');
+        $this->assertSame('moeamen-eltoeuny', Slugify::get('Möamen Eltöuny'));
+
+        Slugify::rule('$', '-dollar');
+        $this->assertSame('500-dollar-bill', slug('500$ bill', '-'));
+        $this->assertSame('500-dollar-bill', slug('500 $ bill', '-'));
     }
 }
